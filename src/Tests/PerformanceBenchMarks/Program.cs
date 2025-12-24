915671b67ae5e70f3fe1341d0fb994804032b6c7
Andrew Markham <andrewmarkham@rocketmail.com>
2025-12-24 20:29:55 +0000

    Refactor ReportApiStreamReader to perform better

---
+++ src/Tests/PerformanceBenchMarks/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+//using System.Diagnostics;
+
+/*
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args, new DebugInProcessConfig());
+*/
+    Console.WriteLine("Hello, World!");
+
+    //Debugger.Launch();
+    //var f = new ProblemJsonFormatterBenchmark();
+    //await f.ReportApiStreamReaderBenchmark();
+    BenchmarkDotNet.Running.BenchmarkRunner.Run<ProblemJsonFormatterBenchmark>();
